36fddbcd74c7de558f315a0ec23aab5060a25ac4
lazybios <lazybios@gmail.com>
2014-05-24 16:19:46 +0800

    add zoom and pan

--- static/js/temp.js
+++ static/js/temp.js
@@ -59,7 +59,6 @@ function mapNodes(nodes){
     return nodesMap;
 } 
 
-
 var scale_x = d3.scale.linear().domain([0,w]).range([0,300]);
 var scale_y = d3.scale.linear().domain([0,h]).range([h,0]);
 
@@ -84,8 +83,21 @@ function render(ppi_json){
                     .append("svg:svg")
                     .attr("class", "stage")
                     .attr("width", w)
-                    .attr("height", h);
+                    .attr("height", h)
+                    .attr("pointer-events", "all")
+                    .append('svg:g')
+                      .call(d3.behavior.zoom().on("zoom", zoom))
+                    .append('svg:g');              
 
+              vis.append("svg:rect")
+                  .attr("class", "overlay")
+                  .attr("width", w)
+                  .attr("height", h);
+
+              function zoom() {
+                  console.log("here", d3.event.translate, d3.event.scale);
+                  vis.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+                }
               var force = d3.layout.force()
                   .nodes(nodes)
                   .links([])
@@ -294,33 +306,15 @@ function render(ppi_json){
 
 
 
-/*              force.on("tick", function(e) {
-                node.attr("transform", function(d, i) {     
-                      if (d.center == 'y'){
-                          return "translate(" + d.x + "," + d.y + ")"; 
-                      } 
-                      return "translate(" + d.x + "," + d.y + ")";                      
-                  });
-
-                  link.attr("x1", function(d)  { return d.source.x; })
-                     .attr("y1", function(d)   { return d.source.y; })
-                     .attr("x2", function(d)   { return d.target.x; })
-                     .attr("y2", function(d)   { return d.target.y; })
-                  });*/
+              vis.style("opacity", 1e-6)
+                        .transition()
+                        .duration(1000)
+                        .style("opacity", 1);
 
                force.on("tick", function(e) {
-                  node.attr("transform", function(d, i) {     
-                      if (d.center == 'y'){
-                          return "translate(" + d.x + "," + d.y + ")"; 
-                      } 
+                  node.attr("transform", function(d, i) {      
                       return "translate(" + d.x + "," + d.y + ")";                      
                   });
-
-/*                  link.attr("x1", function(d)  { return d.source.x; })
-                     .attr("y1", function(d)   { return d.source.y; })
-                     .attr("x2", function(d)   { return d.target.x; })
-                     .attr("y2", function(d)   { return d.target.y; })
-*/
                   link.attr("d", function(d) {
                         var x1 = d.source.x,
                             y1 = d.source.y,
@@ -364,4 +358,4 @@ function render(ppi_json){
                  });
 
               force.start();
-}
+  } 
